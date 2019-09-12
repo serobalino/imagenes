dbcfe5ca461b559c8a71f7d5a7ff92d97c362189
Sebastian <sebastian.robalino@inpsercom.com>
2019-09-12 18:26:21 -0500

    ultima migracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_022805) do
+ActiveRecord::Schema.define(version: 2019_09_12_225822) do
 
   create_table "images", force: :cascade do |t|
-    t.integer "id_im"
+    t.float "peso_im"
+    t.binary "archivo_im"
+    t.string "nombre_im"
+    t.string "ext_im"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "images", "users"
 end
