a348c2548cdac15adeeed95de6efb15030bf739a
Sebastian <sebastian.robalino@inpsercom.com>
2019-09-23 11:03:54 -0500

    rutas de crud de imagenes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/binvenida", to: "home#Index"
+  root to:"home#index"
   get "images/new", to: "images#new"
+  get "images/:id", to: "images#show"
   post "images", to: "images#create"
 end
