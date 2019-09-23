f336f136798df247dc807f19f06e8231087b5de1
Sebastian <sebastian.robalino@inpsercom.com>
2019-09-23 11:03:15 -0500

    crud de imagenes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,8 +3,16 @@ class ImagesController < ApplicationController
     @imagen = Image.new
   end
 
+  def show
+    @imagen = Image.find(params[:id])
+  end
+
+  def index
+    @images = Image.all().limit(10)
+  end
+
   def create
-    @imagen = Image.create(id_im:params[:image][:numero])
-    render json: @imagen
+    # @imagen = Image.create(peso_im:80, archivo_im:params.require(:imagen).permit(:archivo_im), nombre_im:'Archivo', ext_im:'exe', user_id:1)
+    render json: params.require(:imagen).permit(:archivo_im)
   end
 end
