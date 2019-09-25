2325a0b58c5e4f43fedc48fa9b81421b0c625dfd
Sebastian <sebastian.robalino@inpsercom.com>
2019-09-25 18:17:58 -0500

    variables de

---
+++ constants/constantes.js
@@ -0,0 +1,9 @@
+const aplicacion    =   "Lion";
+const backend       =   "http://mateo32115.ml";
+const token         =   {id:3,token:"VfvMC7atUtEvg23GG1s94Yx6mFFA8EspTw9iLsJC"};
+
+export default {
+    aplicacion,
+    backend,
+    token
+}
