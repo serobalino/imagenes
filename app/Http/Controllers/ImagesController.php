23efab25bf86799d7b3ea6772363648a9586b6a8
Sebastian <sebastian.robalino@inpsercom.com>
2019-10-01 18:48:37 -0500

    sin midleare dentro del comtrolador

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -10,11 +10,6 @@ use Illuminate\Support\Facades\Validator;
 
 class ImagesController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     /**
      * Display a listing of the resource.
      *
