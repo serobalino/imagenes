41c1b66965b94b60fb560b3866cb7fe494ead617
Sebastian <sebastian.robalino@inpsercom.com>
2020-01-15 18:14:47 -0500

    cambio sin SSL

--- constants/constantes.js
+++ constants/constantes.js
@@ -1,5 +1,5 @@
 const aplicacion    =   "Lion";
-const backend       =   "https://mateo32115.ml";
+const backend       =   "http://mateo32115.ml";
 const token         =   {id:1,token:"Vc9pbZsDts8TMjC9dXZblh2HAWhFxXn2bb4tOIav"};
 
 export default {
